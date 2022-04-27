7e82322ac9a57abd7e0e3d76aa19729a23b4a63a
ClaraDella <claraevania0@gmail.com>
2022-04-27 21:17:56 +0700

    Perubahan Judul

--- 03-shiny_crud/01_traditional/shiny_app/ui.R
+++ 03-shiny_crud/01_traditional/shiny_app/ui.R
@@ -3,7 +3,7 @@ fluidPage(
   shinyjs::useShinyjs(),
   # Application Title
   titlePanel(
-    h1("Shiny CRUD Application", align = 'center'),
+    h1("Aplikasi Shiny CRUD", align = 'center'),
     windowTitle = "Shiny CRUD Application"
   ),
   cars_table_module_ui("cars_table")
